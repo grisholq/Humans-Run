489d278f1fc3bc7163cfc2b1560c6f32b1fa99db
grisholq <drowen2014@mail.ru>
2021-07-14 21:22:44 +0300

    Commit 14.07
    Микро поправки

--- Assets/Scripts/Platforms/MovingPlatform.cs
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class MovingPlatform : Platform
 {
-    [SerializeField] private Transform start;
-    [SerializeField] private Transform end;
-    [SerializeField] private Transform movingPart;
+    [SerializeField] private Transform _start;
+    [SerializeField] private Transform _end;
+    [SerializeField] private Transform _movingPlatform;
 
     public override void SetState(float state, bool hold)
     {
-        movingPart.transform.position = Vector3.Lerp(start.position, end.position, state);
+        _movingPlatform.transform.position = Vector3.Lerp(_start.position, _end.position, state);
     }
 }
